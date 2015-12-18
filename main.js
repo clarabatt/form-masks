4c5916fb8e2fc3fb19af3b01beb50ae11b32cc29
Clara Verena Battesini <clarabattesini@gmail.com>
2015-12-18 09:59:40 -0300

    A variavel  coleta os dados, a função mascara funciona como esperado, passando para a função cpfFormat.

--- main.js
+++ main.js
@@ -4,15 +4,13 @@
     $cpf.addEventListener( 'input', function(){mascara($cpf, cpfFormat)}, false );
 
     function mascara( objeto, funcao ){
-        var obj = this.value;
-        var func = funcao.value;
-        return function(){
-            obj = fun( obj );   
-            setTimeout( mascara(), 1 );
-        }
+        var obj = objeto.value;
+        var func = funcao;
+        setTimeout( function(){ obj = func( obj ) }, 1 );
     }
 
     function cpfFormat( num ){
+        console.log("ok");
         num = num.replace(/[^0-9]/g,""); // tira os caracteres que não são números 
         num = num.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
         return num

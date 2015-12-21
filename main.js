fd1370a8eaada2e12cb768ea2e0814266952875d
Clara Battesini <clarabattesini@gmail.com>
2015-12-20 21:36:13 -0300

    adicionada uma função exec dentro de mascara que irá retornar o número formatado e depois aribuimos o resultado dessa função ao valor do input.

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 (function( win, doc ){
     'use strict';
     var $cpf = doc.getElementById('cpf');
-    $cpf.addEventListener( 'input', function(){mascara($cpf, cpfFormat)}, false );
+    $cpf.addEventListener( 'input', function(){mascara($cpf, cpfFormat)}, false );    
+
+    var $rg = doc.getElementById('rg');
+    $rg.addEventListener( 'input', function(){mascara($rg, rgFormat)}, false );
 
     function mascara( objeto, funcao ){
         var obj = objeto.value;
         var func = funcao;
-        setTimeout( function(){ obj = func( obj ) }, 1 );
+        function exec(){ 
+            obj = func( obj );
+            return obj;
+        }
+        objeto.value = exec();
     }
 
     function cpfFormat( num ){
-        console.log("ok");
         num = num.replace(/[^0-9]/g,""); // tira os caracteres que não são números 
-        num = num.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+        num = num.replace(/(\d{3})(\d)/,"$1.$2");
+        num = num.replace(/(\d{3})(\d)/,"$1.$2");
+        num = num.replace(/(\d{3})(\d{1,2})/,"$1-$2");
         return num
     }
 
     function rgFormat( num ){
         num = num.replace(/[^0-9]/g,""); // tira os caracteres que não são números 
-        num = num.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+        num = num.replace(/(\d{2})(\d)/,"$1.$2");
+        num = num.replace(/(\d{3})(\d)/,"$1.$2");
+        num = num.replace(/(\d{3})(\d{1,2})/,"$1-$2");
         return num
     }
 })( window, document );
